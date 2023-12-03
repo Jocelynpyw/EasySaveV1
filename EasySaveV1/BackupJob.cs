cd78b6f056a4afe0794ea061b7343c6f34f453cc
jocelyn pyw <jocelynwotcheu2@gmail.com>
2023-12-04 00:02:23 +0100

    Ajoutez des fichiers projet.

---
+++ EasySaveV1/BackupJob.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveV1
+{
+    public class BackupJob
+    {
+        public string JobName { get; set; }
+        public string SourceDirectory { get; set; }
+        public string DestinationDirectory { get; set; }
+        public BackupType BackupType { get; set; }
+        public bool IsActive { get; set; }
+
+        public BackupJob(string jobName , string sourceDirectory , string destinationDirectory )
+        {
+            this.JobName = jobName;
+            this.SourceDirectory = sourceDirectory;
+            this.DestinationDirectory = destinationDirectory;
+        }
+
+        private static void CopyDirectory(string source, string destination)
+        {
+            // Créez le répertoire de destination s'il n'existe pas
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            // Obtenez la liste des fichiers
+            string[] files = Directory.GetFiles(source);
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string destPath = Path.Combine(destination, fileName);
+                File.Copy(file, destPath, true);
+                Console.WriteLine($"Copie du fichier : {fileName}");
+            }
+
+            // Obtenez la liste des sous-répertoires
+            string[] directories = Directory.GetDirectories(source);
+            foreach (string directory in directories)
+            {
+                string dirName = Path.GetFileName(directory);
+                string destPath = Path.Combine(destination, dirName);
+                CopyDirectory(directory, destPath);
+            }
+        }
+        public  void Save()
+        {
+            CopyDirectory(this.SourceDirectory, this.DestinationDirectory);
+        }
+
+    }
+
+    public enum BackupType
+    {
+        Full,
+        Differential
+    }
+}
---
+++ EasySaveV1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace EasySaveV1
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            // Création d'un objet BackupJob
+            BackupJob backupJob = new BackupJob("BackupJob1", @"D:\Mon CF", @"D:\Fichiers de copies");
+
+            // Vérification de l'existence du répertoire source
+            if (!Directory.Exists(backupJob.SourceDirectory))
+            {
+                Console.WriteLine($"Le répertoire source '{backupJob.SourceDirectory}' n'existe pas.");
+                return;
+            }
+
+            // Vérification de l'existence du répertoire destination
+            if (!Directory.Exists(backupJob.DestinationDirectory))
+            {
+                Console.WriteLine($"Le répertoire destination '{backupJob.DestinationDirectory}' n'existe pas.");
+                return;
+            }
+
+            // Affichage des détails du job
+            Console.WriteLine($"Backup Job: {backupJob.JobName}");
+            Console.WriteLine($"Source Directory: {backupJob.SourceDirectory}");
+            Console.WriteLine($"Destination Directory: {backupJob.DestinationDirectory}");
+            Console.WriteLine($"Backup Type: {backupJob.BackupType}");
+            Console.WriteLine($"Is Active: {backupJob.IsActive}");
+            Console.WriteLine();
+
+            // Copie des fichiers en fonction du type de sauvegarde
+            try
+            {
+                if (backupJob.BackupType == BackupType.Full)
+
+                {
+                    backupJob.Save();
+                    Console.WriteLine("Backup complet effectué avec succès.");
+                }
+                else if (backupJob.BackupType == BackupType.Differential)
+                {
+                    // Implémentez ici la logique pour une sauvegarde différentielle
+                    Console.WriteLine("Backup différentiel non implémenté dans cet exemple.");
+                }
+                else
+                {
+                    Console.WriteLine("Type de sauvegarde non pris en charge.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors de la copie : {ex.Message}");
+            }
+        }
+
+        
+    }
+}
